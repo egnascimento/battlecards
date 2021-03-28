06c6565b65cea755bc8ebc00a7fe888a279c1541
Eduardo Nascimento <NascimentoEduardoG@JohnDeere.com>
2021-03-28 01:10:23 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const router = express.Router();
 //Rotas
 const index = require('./routes/index');
 const personRoute = require('./routes/cardRoute');
+app.use(cors())
 app.use('/', index);
 app.use('/persons', personRoute);
 module.exports = app;
