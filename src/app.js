179a926f59b9936dbe5f779e1333b527ffc62679
Eduardo Nascimento <NascimentoEduardoG@JohnDeere.com>
2021-03-28 10:42:09 -0300

    static

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const index = require('./routes/index');
 const personRoute = require('./routes/cardRoute');
 app.use(cors())
+app.use(express.static('img'))
 app.use('/', index);
 app.use('/persons', personRoute);
 module.exports = app;
